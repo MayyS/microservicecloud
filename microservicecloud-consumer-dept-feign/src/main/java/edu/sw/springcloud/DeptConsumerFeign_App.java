fcea9e93f955c3d82760be6dfe452f4b1c5c9005
Swe <864876906@qq.com>
2019-05-14 21:17:27 +0800

    fix bug

--- microservicecloud-consumer-dept-feign/src/main/java/edu/sw/springcloud/DeptConsumerFeign_App.java
+++ microservicecloud-consumer-dept-feign/src/main/java/edu/sw/springcloud/DeptConsumerFeign_App.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients(basePackages= {"edu.sw.springcloud"})
-@ComponentScan("edu.sw.springcloud")
+//@ComponentScan("edu.sw.springcloud")
 public class DeptConsumerFeign_App {
 
     public static void main(String[]args){
