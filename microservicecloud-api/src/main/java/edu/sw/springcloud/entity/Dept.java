df2baf80136e792ed17b1b3b214bc50b6621f954
ssw <8648769006@qq.com>
2019-05-16 11:58:07 +0800

    add servicecommand

--- microservicecloud-api/src/main/java/edu/sw/springcloud/entity/Dept.java
+++ microservicecloud-api/src/main/java/edu/sw/springcloud/entity/Dept.java
@@ -3,26 +3,35 @@ package edu.sw.springcloud.entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @Auther:S
  * @Date:19/5/10
  */
 
-@AllArgsConstructor
 @NoArgsConstructor
+//@AllArgsConstructor
 @Data
-@Accessors(chain = true)
+@Accessors(chain=true)
+@ToString
 public class Dept implements Serializable {
-    private Long deptNo;
+    private int deptNo;
     private String deptName;
     private String dbSource;// 来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库
 
-    public Dept(String dname){
+    public Dept(String dname)
+    {
         super();
-        this.deptName=dname;
+        this.deptName = dname;
     }
+
+
+
 }
